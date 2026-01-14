f1fe4d1865499f264366e3ee98eebbc0a325e7ce
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2026-01-14 19:18:07 +0400

    Add project-card.component.ts

---
+++ src/app/shared/components/project-card/project-card.component.ts
@@ -0,0 +1,3 @@
+import { Component,Input } from '@angular/core';
+@Component({selector:'app-project-card',templateUrl:'./project-card.component.html',styleUrls:['./project-card.component.scss']})
+export class ProjectCardComponent{ @Input() title=''; @Input() description=''; @Input() link=''; }
